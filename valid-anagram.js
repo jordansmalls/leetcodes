dfed1c6049b4c35a118cc8b35259568fe535a604
Jordan Smalls <smalls.jordan@gmail.com>
2025-07-02 15:00:00 -0400

    feat: Solve Valid Anagram using a hash map
    Implemented a solution for Valid Anagram that utilizes a hash map to count character frequencies for efficient comparison.

---
+++ valid-anagram.js
@@ -0,0 +1,39 @@
+// Valid Anagram
+/*
+Given two strings s and t, return true if t is an of s, and false otherwise.
+*/
+
+/**
+ *
+ * @param {string} s
+ * @param {string} t
+ * @return {boolean}
+ */
+
+const isAnagram = function(s, t) {
+    // words that are indeed angrams share the same number of characters
+    if(s.length !== t.length) return false;
+
+    const map = new Map()
+
+    for(const idx of s) {
+        map.set(idx, (map.get(idx) || 0) + 1)
+    }
+
+    for(const char of t) {
+
+        if(!map.has(char)) {
+            return false;
+        } else {
+            map.set(char, (map.get(char) - 1));
+        };
+
+        if(map.get(char) == 0) {
+            map.delete(char)
+        }
+    }
+
+    return map.size == 0
+}
+
+console.log(isAnagram("racecar", "carrace")) // true
