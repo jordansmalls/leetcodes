d78a0a05747cfefeb1dbed78646f4f0b5cc00b0e
Jordan Smalls <smalls.jordan@gmail.com>
2026-02-17 09:16:02 -0500

    add: day 10 solution

---
+++ 30-days-of-js/day10.js
@@ -0,0 +1,26 @@
+/*
+    2666. Allow One Function Call
+
+    Given a function fn, return a new function that is identical to the original function except that it ensures fn is called at most once.
+
+    The first time the returned function is called, it should return the same result as fn.
+    Every subsequent time it is called, it should return undefined.
+*/
+
+/**
+ *
+ * @param {Function} fn
+ * @returns {Function}
+ */
+
+const once = function(fn) {
+    let called = false;
+    return function(...args) {
+        if(called) {
+            return undefined;
+        } else {
+            called = true;
+            return fn(...args)
+        }
+    }
+}
