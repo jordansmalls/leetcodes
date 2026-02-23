07a92ab5150c3c05145a740af548d8943afa1457
Jordan Smalls <smalls.jordan@gmail.com>
2026-02-22 23:37:49 -0500

    add: add day 15 solution

---
+++ 30-days-of-js/day15.js
@@ -0,0 +1,26 @@
+/*
+2725. Interval Cancellation
+
+Given a function fn, an array of arguments args, and an interval time t, return a cancel function cancelFn.
+After a delay of cancelTimeMs, the returned cancel function cancelFn will be invoked.
+
+setTimeout(cancelFn, cancelTimeMs)
+
+The function fn should be called with args immediately and then called again every t milliseconds until cancelFn is called at cancelTimeMs ms.
+*/
+
+
+/**
+ * @param {Function} fn
+ * @param {Array} args
+ * @param {number} t
+ * @return {Function}
+ */
+
+var cancellable = function (fn, args, t) {
+  fn(...args);
+  let timer = setInterval(() => fn(...args), t);
+
+  let cancelFn = () => clearInterval(timer);
+  return cancelFn;
+};
